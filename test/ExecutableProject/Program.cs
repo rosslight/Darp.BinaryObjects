fdd9f0a019672c0fa2ffa8116e4332fcb0f73fb7
Ole Rosskamp <ole.rosskamp@rosslight.de>
2024-11-10 11:04:24 +0100

    refactor: Use generated AllPrimitives for tests

--- test/ExecutableProject/Program.cs
+++ test/ExecutableProject/Program.cs
@@ -1,15 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Darp.BinaryObjects;
-using Test;
+using Test.Asd;
 
 Console.WriteLine("Hello, World!");
 
 OneBool.TryReadLittleEndian(Convert.FromHexString("010002"), out var value);
 int i = 0;
 
-namespace Test
+namespace Test.Asd
 {
     [BinaryObject]
-    public partial record OneBool(bool Value, bool Value2, bool Value3);
+    public partial record struct OneBool(bool Value, ushort Value2, ushort Value3);
 }
